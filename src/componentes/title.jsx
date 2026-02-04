2c9e8a045ff79961cbd70ab3248ea28f79da1724
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-02-04 15:24:57 +0100

    para movil 2

--- src/componentes/title.jsx
+++ src/componentes/title.jsx
@@ -3,7 +3,7 @@ import "../estilos/title.css";
 function Titulo() {
     return ( 
     <div className="contenedor-titulo">
-        <h2><span id="color">OrganizaTe</span>: Organiza y completa tus tareas sin enfuerzo</h2>
+        <h2><span id="color">OrganizaTe</span>: Organiza y completa tus tareas sin esfuerzo</h2>
     </div> )
 };
 
